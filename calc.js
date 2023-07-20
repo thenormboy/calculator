93c2e8e155472ca682a760607755f363a5476544
Bleaze Perris <bperriscode@gmail.com>
2023-07-19 21:53:31 -0400

    added add, subtract, multiply and division functions

---
+++ calc.js
@@ -0,0 +1,38 @@
+let firstNumberDisplay;
+let secondNumberDisplay;
+let operatorDisplay;
+
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+    firstNumber / secondNumber;
+}
+
+function operate(firstNumber, secondNumber, operator) {
+    if (operator == '+') {
+        add(firstNumber, secondNumber);
+    } else if (operator == '-') {
+        subtract(firstNumber, secondNumber);
+    } else if (operator == '*') {
+        multiply(firstNumber, secondNumber);
+    } else if (operator == '/') {
+        divide(firstNumber, secondNumber);
+    }
+}
+
+const buttons = document.querySelectorAll('.buttons');
+const display = document.querySelector('.display');
+
+function displayNumbers() {
+    
+}
